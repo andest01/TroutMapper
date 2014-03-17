e05a9388a0d30c89c0afa565b359a4fc17a45755
standersnerdery <standers@nerdery.com>
2014-03-16 19:29:00 -0500

    Adding StreamList

---
+++ web/assets/scripts/src/views/StreamList.js
@@ -0,0 +1,50 @@
+/**
+ * Created by MBP on 3/16/14.
+ */
+
+define(function(require) {
+    'use strict';
+    var StreamList = function() {
+        this.init();
+    };
+
+    var proto = StreamList.prototype;
+
+    proto.init = function() {
+        
+    };
+
+    proto.setupHandlers = function() {
+
+    };
+
+    proto.createChildren = function() {
+
+    };
+
+    proto.layout = function() {
+
+    };
+
+    proto.enable = function() {
+
+    };
+
+    proto.disable = function() {
+
+    };
+
+    proto.destroy = function() {
+
+    };
+
+    proto.render = function() {
+
+    };
+
+    proto.redraw = function() {
+
+    };
+
+    return StreamList;
+});
